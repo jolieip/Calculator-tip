90da4b8b5ac717feddf7fd00c1a99d8544ed58b6
Jolie Ip <ipjolie1507@gmail.com>
2020-09-11 21:47:07 -0700

    Initial Commit

---
+++ Calculator-tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator-tip
+//
+//  Created by Jolie Ip Ying See on 11/09/2020.
+//  Copyright © 2020 ecotegrity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
